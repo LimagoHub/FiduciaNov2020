1ff72a50c0fd7c8909111d1874c53f57181a85db
limagoHub <limagoHub@limago.de>
2020-11-25 09:37:55 +0100

    initial commit

---
+++ first/src/test/java/de/fiducia/first/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fiducia.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
